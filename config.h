ccf2f3694dfb03f2d6649bad7fe7588a6b165486
Gnaig <tangiang0812@gmail.com>
2022-02-14 23:29:22 +0700

    apply xresource patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int centered = 1;                    /* -c option; centers dmenu on screen */
 static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
+static char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
@@ -18,7 +18,7 @@ static const unsigned int alphas[][3]      = {
        	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
-static const char *colors[SchemeLast][2] = {
+static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
