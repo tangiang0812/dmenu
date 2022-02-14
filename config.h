e4ead123b7e5b6c4728985dd0552b75127315122
Gnaig <tangiang0812@gmail.com>
2022-02-14 22:58:10 +0700

    apply alpha patch and center patch

--- config.h
+++ config.h
@@ -2,11 +2,22 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 1000;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+
+static const unsigned int baralpha = 0xd9;
+static const unsigned int borderalpha = OPAQUE;
+static const unsigned int alphas[][3]      = {
+       	/*               fg      bg        border     */
+       	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+       	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
+
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
@@ -14,7 +25,7 @@ static const char *colors[SchemeLast][2] = {
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 10;
 
 /*
  * Characters not considered part of a word while deleting words
